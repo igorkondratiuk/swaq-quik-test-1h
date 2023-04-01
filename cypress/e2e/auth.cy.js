a997470701f39f7cf7b18bd36bdeeeb51b8e562c
papa <learninpasv@gmail.com>
2023-04-01 11:58:52 -0400

    upd cypress.config.js,.env

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Auth', () => {
+  it('auth with valid data', () => {
+    cy.visit('https://saucedemo.com')
+    cy.get('[data-test="username"]')
+    cy.get('[data-test="password"]')
+    cy.get('[data-test="login-button"]').click()
   })
 })
