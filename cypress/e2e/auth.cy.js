222011437a12b7303700647224568c3b483a9ac7
papa <learninpasv@gmail.com>
2023-04-01 12:58:41 -0400

    upd 2 auth.cy.js

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,8 +1,16 @@
+
 describe('Auth', () => {
+  beforeEach('',()=>{
+    cy.visit('')
+  })
+
   it('auth with valid data', () => {
-    cy.visit('https://www.saucedemo.com')
+
     cy.get('[data-test="username"]').type('standard_user')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('[data-test="login-button"]').click()
+
+    cy.location('pathname').should('eq','/inventory.html')
+    cy.get('#shopping_cart_container').should('exist').should('be.visible')
   })
 })
